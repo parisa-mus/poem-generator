511704024158a03f4c372222cde869257a9b902a
parisa-mus <pm0991829@gmail.com>
2025-10-18 19:25:12 +0330

    API Integration

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: "به نام خداوند جان و خرد",
+function displayPoem(response) {
+  new Typewriter(".poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "bdb6ff86a835be877o4tc8baf0ebd69b";
+  let context =
+    "You are a  persian poem expert.make sure write a poem in 4 verse.wrire a poem with srrong meaning ";
+  let prompt = `User instructions: Generate a Persian poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
